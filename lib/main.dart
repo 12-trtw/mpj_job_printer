e8d847a3c6ea6b59985e6dde0d32f55f7f37fb99
Tharathorn Wongka-in <>
2026-07-24 14:26:17 +0700

    Fix: Bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:mpj_job_printer/features/print/presentation/screen/dashboard_screen.dart';
-import 'package:window_manager/window_manager.dart';
+import 'package:mpj_job_printer/src/features/presentation/views/dashboard_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await initializeDateFormatting('th_TH', null);
-
-  await windowManager.ensureInitialized();
-
-  WindowOptions windowOptions = const WindowOptions(
-    size: Size(1100, 750),
-    minimumSize: Size(950, 600),
-    center: true,
-    title: 'MPJ Job Order Form Printer (Flutter Native Win32)',
-    backgroundColor: Colors.transparent,
-    skipTaskbar: false,
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
   );
-
-  await windowManager.waitUntilReadyToShow(windowOptions, () async {
-    await windowManager.show();
-    await windowManager.focus();
-  });
-
-  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -34,12 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MPJ Job Printer',
+      title: 'MPJ Print Dashboard',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2563EB)),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1E3A8A)),
         useMaterial3: true,
-        fontFamily: 'Segoe UI',
       ),
       home: const DashboardScreen(),
     );
